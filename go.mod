56e79139ac175980401dfe25989218f892328a6d
jfeng986 <jfeng986@gmail.com>
2023-08-18 14:18:40 +0800

    feat: add chunked upload

--- go.mod
+++ go.mod
@@ -16,7 +16,6 @@ require (
 	golang.org/x/crypto v0.11.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.3
-	xorm.io/xorm v1.3.2
 )
 
 require (
@@ -42,20 +41,15 @@ require (
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/openzipkin/zipkin-go v0.4.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
@@ -63,7 +57,6 @@ require (
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
-	github.com/syndtr/goleveldb v1.0.0 // indirect
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/exporters/jaeger v1.14.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.14.0 // indirect
@@ -83,5 +76,4 @@ require (
 	google.golang.org/grpc v1.56.2 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 )
--- utils/upload_util.go
+++ utils/upload_util.go
@@ -1,38 +1,103 @@
 package utils
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"log"
 	"net/http"
 	"path"
+	"time"
 
 	"go-zero-cloud-disk/core/define"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
+
+const PartSize = 5 * 1024 * 1024 // 5MB
 
 func AWSUpload(r *http.Request) (string, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(define.Region))
 	if err != nil {
-		log.Fatal("err: ", err)
+		log.Fatal("Unable to load SDK config", err)
 		return "", err
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	client := s3.NewFromConfig(cfg)
 
 	file, fileHeader, err := r.FormFile("file")
-	key := "cloud-disk/" + GenUUID() + path.Ext(fileHeader.Filename)
-
-	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(define.AWSBucket),
-		Key:    aws.String(key),
-		Body:   file,
-	})
 	if err != nil {
-		log.Fatal("failed to upload file, ", err)
+		log.Fatal("Unable to get form file", err)
 		return "", err
 	}
+	defer file.Close()
+
+	key := "cloud-disk/" + GenUUID() + path.Ext(fileHeader.Filename)
+
+	// 1. Initialize the multipart upload
+	createResp, err := client.CreateMultipartUpload(ctx, &s3.CreateMultipartUploadInput{
+		Bucket: aws.String(define.AWSBucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		log.Fatal("Failed to create multipart upload", err)
+		return "", err
+	}
+
+	var completedParts []types.CompletedPart
+	partNum := int32(1)
+
+	buffer := make([]byte, PartSize)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Fatal("Failed to read file part", err)
+			return "", err
+		}
+		if n == 0 {
+			break
+		}
+
+		// 2. Upload each part
+		uploadResp, err := client.UploadPart(ctx, &s3.UploadPartInput{
+			Bucket:     aws.String(define.AWSBucket),
+			Key:        aws.String(key),
+			PartNumber: partNum,
+			UploadId:   aws.String(*createResp.UploadId),
+			Body:       bytes.NewReader(buffer[:n]),
+		})
+		if err != nil {
+			log.Fatal("Failed to upload part", err)
+			return "", err
+		}
+
+		completedParts = append(completedParts, types.CompletedPart{
+			ETag:       uploadResp.ETag,
+			PartNumber: partNum,
+		})
+
+		partNum++
+	}
+
+	// 3. Complete the multipart upload
+	_, err = client.CompleteMultipartUpload(ctx, &s3.CompleteMultipartUploadInput{
+		Bucket:   aws.String(define.AWSBucket),
+		Key:      aws.String(key),
+		UploadId: aws.String(*createResp.UploadId),
+		MultipartUpload: &types.CompletedMultipartUpload{
+			Parts: completedParts,
+		},
+	})
+	if err != nil {
+		log.Fatal("Failed to complete multipart upload", err)
+		return "", err
+	}
+
 	return define.AWSBucket + "/" + key, nil
 }
